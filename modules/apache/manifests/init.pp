5bf665afb7b27faa9a498ce35d3114d95ec73a62
Jason Kim <jason.kim.jiho@gmail.com>
2012-05-01 01:12:50 -0400

    fixed apache2 error - added requirement on virtualhost file

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -8,18 +8,19 @@ class apache {
   file { "/etc/apache2/mods-enabled/rewrite.load":
     ensure => link,
     target => "/etc/apache2/mods-available/rewrite.load",
-    require => Package["apache2"],
+    require => Package["apache2"]
   }
 
   file { "/etc/apache2/sites-available/vagrant_webroot":
     ensure => present,
     source => "/vagrant/manifests/vagrant_webroot",
+    require => Package["apache2"]
   }
 
   file { "/etc/apache2/sites-enabled/vagrant_webroot":
     ensure => link,
     target => "/etc/apache2/sites-available/vagrant_webroot",
-    require => Package["apache2"],
+    require => File["/etc/apache2/sites-available/vagrant_webroot"]
   }
 
   # starts the apache2 service once the packages installed, and monitors changes to its configuration files and reloads if nesessary
